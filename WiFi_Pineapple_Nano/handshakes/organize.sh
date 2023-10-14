03b4b59be72555f45f7696fb5d9ec5d453621a2f
Juliano Borba <juliano.sato@protonmail.com>
2023-10-13 23:50:01 -0300

    Update organize.sh

--- WiFi_Pineapple_Nano/handshakes/organize.sh
+++ WiFi_Pineapple_Nano/handshakes/organize.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 # rename files with the date of it's creation
-for f in *cap; do mv -n "$f" "$(date -r "$f" +"%Y%m%d_%H%M%S").pcap"; done
+for f in PW-NA-GO-TC-HI*.pcap; do mv -n "$f" "$(date -r "$f" +"%Y%m%d_%H%M%S").pcap"; done
+for f in AI-RO-DU-MP*.netxml; do mv -n "$f" "$(date -r "$f" +"%Y%m%d_%H%M%S").netxml"; done
