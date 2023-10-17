60607c13abb7be4f8b7349593fce6124213f5665
Juliano Borba <juliano.sato@protonmail.com>
2023-10-17 01:50:26 -0300

    Update script.js

--- WiFi_Pineapple_Nano/bettercap_warwalking/script.js
+++ WiFi_Pineapple_Nano/bettercap_warwalking/script.js
@@ -1,47 +1,53 @@
 var accessPoint = {};
 
-function exportEvent(event) {
+function exportEvent(data, isNewClientEvent) {
 
     var gps = session.GPS;
-
     if (gps.Latitude === 0 || gps.Longitude === 0) {
         return;
     }
 
-    if (!accessPoint[event.data.mac]) {
-        accessPoint[event.data.mac] = {
+    if (!accessPoint[data.mac]) {
+        accessPoint[data.mac] = {
             seen: 0,
             lastSeen: '',
             clients: []
         };
     }
 
-    accessPoint[event.data.mac].seen++;
-    accessPoint[event.data.mac].lastSeen = event.data.last_seen;
-    accessPoint[event.data.mac].clients = accessPoint[event.data.mac].clients.concat(event.data.clients);
+    if (!isNewClientEvent) {
+        accessPoint[data.mac].seen++;
+    }
+    accessPoint[data.mac].lastSeen = data.last_seen;
+    accessPoint[data.mac].clients = accessPoint[data.mac].clients.concat(data.clients);
 
     var capture = {
-        Hostname: event.data.hostname,
-        Mac: event.data.mac,
-        Updated: accessPoint[event.data.mac].lastSeen,
+        Hostname: data.hostname,
+        Mac: data.mac,
+        Updated: accessPoint[data.mac].lastSeen,
         Latitude: gps.Latitude,
         Longitude: gps.Longitude,
         Altitude: gps.Altitude,
-        Seen: accessPoint[event.data.mac].seen,
-        Clients: accessPoint[event.data.mac].clients,
-        Encryption: event.data.encryption + '+' + event.data.authentication + '-' + event.data.cipher,
-        Packets: event.data.received,
-        Manufacturer: event.data.vendor
+        Seen: accessPoint[data.mac].seen,
+        Clients: accessPoint[data.mac].clients,
+        Encryption: data.encryption + '+' + data.authentication + '-' + data.cipher,
+        Packets: data.received,
+        Manufacturer: data.vendor,
+        WPS: data.wps
     };
 
-    var pHostname = event.data.hostname.replace(/[/\\?%7*:|"<>._ ]/g, '');
-    var pMac = event.data.mac.replace(/:/g, '');
-    var path = '/sd/LEET/handshakes/' + pHostname + '_' + pMac + '.gps.json';
+    var pHostname = data.hostname.replace(/[/\\?%7*:|"<>._ ]/g, '');
+    var pMac = data.mac.replace(/:/g, '');
+    var path = '/sd/LEET/handshakes/bettercap_gps_json' + pHostname + '_' + pMac + '.gps.json';
 
     writeFile(path, JSON.stringify(capture));
 
 }
 
 onEvent('wifi.ap.new', function(event) {
-    exportEvent(event);
+    exportEvent(event.data, false);
+});
+
+onEvent('wifi.client.new', function(event) {
+    exportEvent(event.data['AP'], true);
 });
