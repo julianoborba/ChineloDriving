4d2e56b8270cd3fa82f138bb2de2f0f3db21d939
Juliano Borba <juliano.sato@protonmail.com>
2023-10-18 08:59:17 -0300

    Update pack_to_download.sh

--- WiFi_Pineapple_Nano/handshakes/pack_to_download.sh
+++ WiFi_Pineapple_Nano/handshakes/pack_to_download.sh
@@ -2,3 +2,5 @@
 # zip in a way to be downloadable in PineAP interface
 zip -r BE-TT-ER-CA-PZ-IP_full.pcap bettercap_gps_json
 zip -r AI-RO-DU-MP-ZI-P0_full.pcap airodump_ng_netxml
+rm -rf bettercap_gps_json/*
+rm -rf airodump_ng_netxml/*
