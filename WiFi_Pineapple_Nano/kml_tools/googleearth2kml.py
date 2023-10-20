98f2be239b46e36c5509fba77bd88ff2b09119d9
Juliano Borba <juliano.sato@protonmail.com>
2023-10-20 15:03:20 -0300

    Update googleearth2kml.py

--- WiFi_Pineapple_Nano/kml_tools/googleearth2kml.py
+++ WiFi_Pineapple_Nano/kml_tools/googleearth2kml.py
@@ -1,28 +1,54 @@
 #!/usr/bin/env python3
 
-# # #
-# Refers to
-# https://github.com/dreadnought
-# Patrick Salecker <mail@salecker.org>
-# https://www.salecker.org/software/netxml2kml.html
-# # #
-
 import optparse
 import os
 import re
+import math
 from bs4 import BeautifulSoup
+
+
+# GPT generated
+def obtain_gps_avg(coordinates):
+
+    if not coordinates:
+        return None, None
+
+    total_x = 0
+    total_y = 0
+    total_z = 0
+
+    for lat, lon in coordinates:
+        lat_rad = math.radians(lat)
+        lon_rad = math.radians(lon)
+
+        # Convert latitude and longitude to Cartesian coordinates
+        total_x += math.cos(lat_rad) * math.cos(lon_rad)
+        total_y += math.cos(lat_rad) * math.sin(lon_rad)
+        total_z += math.sin(lat_rad)
+
+    avg_x = total_x / len(coordinates)
+    avg_y = total_y / len(coordinates)
+    avg_z = total_z / len(coordinates)
+
+    # Convert the average Cartesian coordinates back to latitude and longitude
+    avg_lon = math.atan2(avg_y, avg_x)
+    hyp = math.sqrt(avg_x**2 + avg_y**2)
+    avg_lat = math.atan2(avg_z, hyp)
+
+    # Convert latitude and longitude back to degrees and return
+    return math.degrees(avg_lat), math.degrees(avg_lon)
 
 
 def parse_placemark_node(node):
     
     styleurl = node.find('styleurl').string
     name = node.find('name').string
-    desc = node.find('description').string
+    description = node.find('description').string
     coordinates = node.find('coordinates').string
     return {
         'styleurl': styleurl,
         'name': name,
-        'description': desc,
+        'description': description,
         'coordinates': coordinates
     }
 
@@ -35,34 +61,28 @@ def parse_kml(filepath):
     with open(filepath, encoding='latin-1') as file:
         soup = BeautifulSoup(file, 'lxml')
 
-    placemarks = [parse_placemark_node(p) for p in soup.find_all('placemark')]
-    return placemarks
+    return [parse_placemark_node(placemark) for placemark in soup.find_all('placemark')]
 
 
 def get_file_list(path):
 
     pattern = re.compile('.*\.kml$')
-    files = [f for f in os.listdir(path) if pattern.match(f)]
-    return files
+    return [f for f in os.listdir(path) if pattern.match(f)]
 
 
 def merge_data(data1, data2):
 
+    print(f'[*] Merging [{data1["name"]}] AP data seen {data1["seen"]} times')
     raw_lon_1 = data1['coordinates'].split(',')[0]
     raw_lon_2 = data2['coordinates'].split(',')[0]
     raw_lat_1 = data1['coordinates'].split(',')[1]
     raw_lat_2 = data2['coordinates'].split(',')[1]
-
-    raw_lon_1 = "{:.6f}".format(float(raw_lon_1))
-    raw_lon_2 = "{:.6f}".format(float(raw_lon_2))
-    raw_lat_1 = "{:.6f}".format(float(raw_lat_1))
-    raw_lat_2 = "{:.6f}".format(float(raw_lat_2))
-
-    lon = "{:.6f}".format(float(raw_lon_1) + float(raw_lon_2))
-    lat = "{:.6f}".format(float(raw_lat_1) + float(raw_lat_2))
-
-    data1['coordinates'] = f'{float(lon)},{float(lat)}'
-
+    coordinates = [
+        (float(raw_lat_1), float(raw_lon_1)),
+        (float(raw_lat_2), float(raw_lon_2))
+    ]
+    avg_latitude, avg_longitude = obtain_gps_avg(coordinates)
+    data1['coordinates'] = f'{float(avg_longitude)},{float(avg_latitude)}'
     return data1
 
 
@@ -107,50 +127,42 @@ def generate_klm(placemarks, out):
     doc.append(generate_style(soup, 'clear', 'https://raw.githubusercontent.com/julianoborba/ChineloDriving/main/WiFi_Pineapple_Nano/kml_tools/clear.png'))
     doc.append(generate_style(soup, 'clients', 'https://raw.githubusercontent.com/julianoborba/ChineloDriving/main/WiFi_Pineapple_Nano/kml_tools/clients.png'))
 
-    for k, p in placemarks.items():
-        pm = soup.new_tag('Placemark')
+    for k, placemark in placemarks.items():
+        new_placemark = soup.new_tag('Placemark')
 
         name = soup.new_tag('name')
-        name.string = f'{p["name"]}'
+        name.string = f'{placemark["name"]}'
 
         description = soup.new_tag('description')
-        description.string = f'{p["description"]}'
+        description.string = f'{placemark["description"]}'
 
-        pt = soup.new_tag('Point')
-        coo = soup.new_tag('coordinates')
+        point = soup.new_tag('Point')
+        coordinates = soup.new_tag('coordinates')
 
-        split_coordinates = p['coordinates'].split(',')
-        raw_lon = split_coordinates[0]
-        raw_lat = split_coordinates[1]
-        if p['seen'] > 1:
-            lon = float(raw_lon) / p['seen']
-            lat = float(raw_lat) / p['seen'] 
-            coo.string = f'{"{:.6f}".format(lon)},{"{:.6f}".format(lat)}'
-        else:
-            coo.string = f'{"{:.6f}".format(float(raw_lon))},{"{:.6f}".format(float(raw_lat))}'
+        coordinates.string = placemark['coordinates']
 
-        stu = soup.new_tag('styleUrl')
-        if '#standard' in f'{p["styleurl"]}':
-            stu.string = '#standard'
-        if '#open' in f'{p["styleurl"]}':
-            stu.string = '#open'
-        if '#clear' in f'{p["styleurl"]}':
-            stu.string = '#clear'
-        if '#clients' in f'{p["styleurl"]}':
-            stu.string = '#clients'
+        style_url = soup.new_tag('styleUrl')
+        if '#standard' in f'{placemark["styleurl"]}':
+            style_url.string = '#standard'
+        if '#open' in f'{placemark["styleurl"]}':
+            style_url.string = '#open'
+        if '#clear' in f'{placemark["styleurl"]}':
+            style_url.string = '#clear'
+        if '#clients' in f'{placemark["styleurl"]}':
+            style_url.string = '#clients'
 
-        pt.append(coo)
-        pm.append(stu)
-        pm.append(name)
+        point.append(coordinates)
+        new_placemark.append(style_url)
+        new_placemark.append(name)
 
         # set placemark visibitily, 0 is invisible
         visibility = soup.new_tag('visibility')
         visibility.string = '0'
         # pm.append(visibility)
 
-        pm.append(description)
-        pm.append(pt)
-        doc.append(pm)
+        new_placemark.append(description)
+        new_placemark.append(point)
+        doc.append(new_placemark)
 
     kml.append(doc)
     soup.append(kml)
@@ -159,6 +171,10 @@ def generate_klm(placemarks, out):
        file.write(str(soup))
 
 
+# Refers to
+# https://github.com/dreadnought
+# Patrick Salecker <mail@salecker.org>
+# https://www.salecker.org/software/netxml2kml.html
 def main():
 
     parser = optparse.OptionParser('usage%prog -d <kml directory> -o <output file>')
@@ -174,22 +190,22 @@ def main():
         exit(0)
 
     files = get_file_list(dirpath)
-    pmlist = [parse_kml(os.path.join(dirpath, f)) for f in files]
+    placemark_list = [parse_kml(os.path.join(dirpath, file)) for file in files]
 
-    unique_places = {}
-    for pl in pmlist:
-        for p in pl:
-            if p and 'name' in p and p['name']:
-                if p['name'] not in unique_places:
-                    unique_places[p['name']] = p
-                    unique_places[p['name']]['seen'] = 1
+    unique_placemark = {}
+    for placemarks in placemark_list:
+        for placemark in placemarks:
+            if placemark and 'name' in placemark and placemark['name']:
+                if placemark['name'] not in unique_placemark:
+                    unique_placemark[placemark['name']] = placemark
+                    unique_placemark[placemark['name']]['seen'] = 1
                 else:
-                    unique_places[p['name']]['seen'] = unique_places[p['name']]['seen'] + 1
-                    unique_places[p['name']] = merge_data(
-                        unique_places[p['name']], p
+                    unique_placemark[placemark['name']]['seen'] = unique_placemark[placemark['name']]['seen'] + 1
+                    unique_placemark[placemark['name']] = merge_data(
+                        unique_placemark[placemark['name']], placemark
                     )
 
-    generate_klm(unique_places, out)
+    generate_klm(unique_placemark, out)
 
 
 if __name__ == '__main__':
