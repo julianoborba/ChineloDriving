dd905126534d36787b559969f1d08aa08a1ea82f
Juliano Borba <juliano.sato@protonmail.com>
2023-10-20 00:44:24 -0300

    Update bettercap2kml.py

--- WiFi_Pineapple_Nano/kml_tools/bettercap2kml.py
+++ WiFi_Pineapple_Nano/kml_tools/bettercap2kml.py
@@ -10,27 +10,6 @@ from bs4 import BeautifulSoup
 
 
 # GPT generated
-def haversine(lat1, lon1, lat2, lon2):
-
-    # Radius of the Earth in kilometers
-    radius = 6371.0
-
-    # Convert latitude and longitude from degrees to radians
-    lat1, lon1, lat2, lon2 = map(math.radians, [lat1, lon1, lat2, lon2])
-
-    # Haversine formula
-    dlat = lat2 - lat1
-    dlon = lon2 - lon1
-    a = math.sin(dlat/2)**2 + math.cos(lat1) * math.cos(lat2) * math.sin(dlon/2)**2
-    c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
-
-    # Calculate the distance
-    distance = radius * c
-
-    return distance
-
-
-# GPT generated
 def gps_avg(coordinates):
 
     if not coordinates:
