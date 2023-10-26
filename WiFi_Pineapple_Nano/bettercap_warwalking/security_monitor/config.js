efb4f6bd7e1cef97ead855a9a928833e6b40592e
Juliano Borba <juliano.sato@protonmail.com>
2023-10-26 13:04:48 -0300

    Create config.js

---
+++ WiFi_Pineapple_Nano/bettercap_warwalking/security_monitor/config.js
@@ -0,0 +1,15 @@
+var wifiInterface = 'wlan1mon';
+
+var notifyUknownEndpoints = true;
+var notifyRogueAPs = true;
+var notifyDeauth = true;
+var notifyGatewayChanges = true;
+
+var telegramEnabled = false;
+var telegramToken = '';
+var telegramChatId = '';
+
+var discordEnabled = true;
+var discordWebHook = '';
+
+var dbPath = '/sd/LEET/handshakes/betsecmon.json';
